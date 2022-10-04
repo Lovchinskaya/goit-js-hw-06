871d43f375e9d52d6925ad48bc09de8242db61d3
Lovchinskaya <katia.lovchinskaya@gmail.com>
2022-10-04 21:00:22 +0300

    HW  6 task2

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,20 @@
 //1.
-const catagoryArray = document.querySelectorAll('li.item');
-const numberCatagory = catagoryArray.length;
+const categoryArray = document.querySelectorAll('.item');
+const numberCatagory = categoryArray.length;
 console.log(`Number of categories: ${numberCatagory}`);
-//2. 
+//2.
+categoryArray.forEach(category => {
+    const group = [...category.children];
+    group.forEach(element => {
+        if (element.tagName === 'H2') {
+            console.log(`Elements: ${element.textContent}`);
+        }
+    });
+    group.forEach(element => {
+        if (element.tagName === 'UL') {
+            console.log(`Elements: ${element.children.length}`);
+        }
+    });
+    console.log(' ');
+});
+
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listEl = ingredients.map(title => {
+  const  listElement = document.createElement('li');
+  listElement.textContent = title;
+  listElement.classList.add("item");
+  return listElement;
+});
+console.log(listEl);
+
+const ingredientsPart = document.querySelector('#ingredients');
+ingredientsPart.append(...listEl);
