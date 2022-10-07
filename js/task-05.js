4edb4306e4db8da805a88c8e7631c0669865ccb7
Lovchinskaya <katia.lovchinskaya@gmail.com>
2022-10-07 19:49:01 +0300

    HW 6 task 5

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const textInput = document.querySelector('#name-input');
+const output = document.querySelector("#name-output");
+
+    
+const inputValue = function (event) {
+    if (event.currentTarget.value !== "") {
+          output.textContent = event.currentTarget.value;
+    } else {
+         textOutputEl.textContent = "Anonymous";
+      }
+  
+};
+
+textInput.addEventListener('input', inputValue);
