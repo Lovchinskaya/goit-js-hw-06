56ffe0d5e952ea55ff49664bcb1d299334f583e6
Lovchinskaya <katia.lovchinskaya@gmail.com>
2022-10-09 19:07:03 +0300

    WH 6 task8 /2

--- js/task-08.js
+++ js/task-08.js
@@ -9,7 +9,7 @@ function formSubmit(event) {
   } = event.currentTarget;
 
   if (email.value === '' || password.value === '') {
-    return console.log("Please fill in all the fields!");
+    alert ("Please fill in all the fields!");
   } 
   console.log(`Login: ${email.value}, Password: ${password.value}`);
   event.currentTarget.reset();
